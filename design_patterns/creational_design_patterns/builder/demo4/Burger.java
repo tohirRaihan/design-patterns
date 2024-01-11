be73fcd8b136b18a3876231d2f4608091ae23fc8
Tohir Raihan <tohir.raihan@gmail.com>
2024-01-12 00:11:44 +0600

    :pencil2: Add: demo4 for Builder design pattern

---
+++ design_patterns/creational_design_patterns/builder/demo4/Burger.java
@@ -0,0 +1,75 @@
+package design_patterns.creational_design_patterns.builder.demo4;
+
+class Burger {
+    
+    private String size;
+    private boolean egg;
+    private boolean extraCheese;
+    private boolean mayonese;
+    private boolean onion;
+    private boolean lettuce;
+    
+    private Burger(BurgerBuilder burgerBuilder) {
+        // Initialize all fields and you can also add validations
+        this.size = burgerBuilder.size;
+        this.egg = burgerBuilder.egg;
+        this.extraCheese = burgerBuilder.extraCheese;
+        this.mayonese = burgerBuilder.mayonese;
+        this.onion = burgerBuilder.onion;
+        this.lettuce = burgerBuilder.lettuce;
+    }
+
+    public String getSize() {return size;}
+    public boolean isEgg() {return egg;}
+    public boolean isExtraCheese() {return extraCheese;}
+    public boolean isMayonese() {return mayonese;}
+    public boolean isOnion() {return onion;}
+    public boolean isLettuce() {return lettuce;}
+
+    public static class BurgerBuilder {
+        private String size;
+        private boolean egg;
+        private boolean extraCheese;
+        private boolean mayonese;
+        private boolean onion;
+        private boolean lettuce;
+
+        public BurgerBuilder size(String size) {
+            this.size = size;
+            return this;
+        }
+
+        public BurgerBuilder egg(boolean egg) {
+            this.egg = egg;
+            return this;
+        }
+
+        public BurgerBuilder extraCheese(boolean extraCheese) {
+            this.extraCheese = extraCheese;
+            return this;
+        }
+
+        public BurgerBuilder mayonese(boolean mayonese) {
+            this.mayonese = mayonese;
+            return this;
+        }
+
+        public BurgerBuilder onion(boolean onion) {
+            this.onion = onion;
+            return this;
+        }
+
+        public BurgerBuilder lettuce(boolean lettuce) {
+            this.lettuce = lettuce;
+            return this;
+        }
+
+        public Burger build() {return new Burger(this);}
+    }
+
+    @Override
+    public String toString() {
+        return "Burger [size=" + size + ", egg=" + egg + ", extraCheese=" + extraCheese + ", mayonese=" + mayonese
+                + ", onion=" + onion + ", lettuce=" + lettuce + "]";
+    }
+}
---
+++ design_patterns/creational_design_patterns/builder/demo4/Client.java
@@ -0,0 +1,8 @@
+package design_patterns.creational_design_patterns.builder.demo4;
+
+class Client {
+    public static void main(String[] args) {
+        Burger burger = new Burger.BurgerBuilder().size("LARGE").mayonese(true).egg(true).extraCheese(false).onion(true).lettuce(false).build();
+        System.out.println(burger.toString());
+    }
+}
